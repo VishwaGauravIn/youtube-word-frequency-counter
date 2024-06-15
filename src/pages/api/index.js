f61035726058c14c79bf555e42073647470cd3d7
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-06-15 18:54:12 +0530

    api improvements

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,19 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getCaptionUrl } from "@/utils/microUtils";
 import ytdl from "ytdl-core";
 
 export default async function handler(req, res) {
   const videoId = req.query.id;
   let info = await ytdl.getInfo(videoId);
-  let captionUrl =
-    info.player_response.captions.playerCaptionsTracklistRenderer
-      .captionTracks[0].baseUrl;
+  let captionArray =
+    info.player_response.captions.playerCaptionsTracklistRenderer.captionTracks;
   let videoDetails = info.player_response.videoDetails;
 
   // Set cache headers for 1 day (86400 seconds)
   res.setHeader("Cache-Control", "public, max-age=86400");
 
   res.json({
-    videoDetails: videoDetails,
-    captionUrl: captionUrl,
+    videoDetails: {
+      videoId: videoDetails.videoId,
+      title: videoDetails.title,
+      thumbnail: {
+        thumbnails: [videoDetails.thumbnail.thumbnails[0]],
+      },
+    },
+    captionUrl: getCaptionUrl(captionArray),
   });
 }
