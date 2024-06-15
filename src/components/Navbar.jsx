e3bca26ef8d8ccfc112e534007bc57d780ce5bba
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-06-15 08:45:25 +0530

    added github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,15 @@ export default function Navbar() {
         <Youtube className="h-10 w-10 stroke-[0.5]" />
         <span className="sm:block hidden"> YouTube Word Frequency Counter</span>
       </Link>
-      <Button variant="outline">
-        <Github className="mr-2 h-4 w-4" /> GitHub
-      </Button>
+      <Link
+        href="https://github.com/VishwaGauravIn/youtube-word-frequency-counter"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Button variant="outline">
+          <Github className="mr-2 h-4 w-4" /> GitHub
+        </Button>
+      </Link>
     </nav>
   );
 }
